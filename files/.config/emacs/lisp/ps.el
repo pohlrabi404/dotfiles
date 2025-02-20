107b4448d84671ef038de4092bde5c7558dd7fa4
Pohlrabi <pohlrabi@gmail.com>
2025-02-20 11:56:20 +0900

    fix(ps.el) fix missing version

--- files/.config/emacs/lisp/ps.el
+++ files/.config/emacs/lisp/ps.el
@@ -2,7 +2,8 @@
 ;; disable package.el
 (setq package-enable-at-startup nil)
 
-)
+(defvar elpaca-installer-version 0.9)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
