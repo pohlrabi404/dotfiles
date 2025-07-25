6b1893e9f5596c37ece375727f00a0929dcebdd8
Pohlrabi <pohlrabi404@gmail.com>
2025-07-25 21:24:36 +0900

    a nvim pkgmanager

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,2 +1,36 @@
+vim.pack.add({
+    -- plugin manager
+    "https://github.com/folke/lazy.nvim.git",
+    -- colorscheme: gruber-darker
+    "https://github.com/blazkowolf/gruber-darker.nvim",
+})
+
+vim.cmd.colorscheme("gruber-darker")
+vim.g.mapleader = " "
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "gruber-darker" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+  performance = {
+      rtp = {
+	  disabled_plugins = {
+	      "gzip",
+	      "matchit",
+	      "matchparen",
+	      "tarPlugin",
+	      "zipPlugin",
+	      "tutor",
+	      "netrwPlugin",
+	  },
+      },
+  },
+})
+
 require("settings")
 require("autocmds/auto-ssh")
