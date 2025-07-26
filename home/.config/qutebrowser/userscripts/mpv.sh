e51e73f88fa26f1ca9d30726836b6e2a8349dbb4
Pohlrabi <pohlrabi404@gmail.com>
2025-07-26 15:08:07 +0900

    m mpv qutebrowser scripts

--- home/.config/qutebrowser/userscripts/mpv.sh
+++ home/.config/qutebrowser/userscripts/mpv.sh
@@ -8,7 +8,7 @@ SOCKET="${XDG_RUNTIME_DIR:-/tmp}/mpv-socket"
 
 # ------------------------------------------------------------------ functions
 start_mpv() {
-    exec mpv --input-ipc-server="$SOCKET" "$QUTE_URL"
+    exec mpv --force-window --input-ipc-server="$SOCKET" "$QUTE_URL"
 }
 
 send_to_playlist() {
