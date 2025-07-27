d690695e0348ce0c8de97923ee9166dde8ba4715
Pohlrabi <pohlrabi404@gmail.com>
2025-07-28 07:15:26 +0900

    m blink config

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -15,17 +15,33 @@ return {
 		---@type blink.cmp.Config
 		opts = {
 			keymap = { preset = "default" },
-
 			appearance = {
 				nerd_font_variant = "mono",
 			},
-
-			-- (Default) Only show the documentation popup when manually triggered
-			completion = { documentation = { auto_show = false } },
+			signature = { enabled = true, },
+			completion = {
+				documentation = { auto_show = true, auto_show_delay_ms = 500 },
+				accept = {
+					create_undo_point = true,
+					resolve_timeout_ms = 100,
+					auto_brackets = {
+						enabled = true,
+						default_brackets = { "(", ")", "[", "]", "{", "}" },
+						-- default_brackets = { "(", ")" },
+						semantic_token_resolution = {
+							enabled = true,
+							timeout_ms = 400,
+						},
+					},
+				},
+			},
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer" },
 			},
 			fuzzy = { implementation = "prefer_rust_with_warning" },
+			term = {
+				enabled = true,
+			},
 		},
 		opts_extend = { "sources.default" },
 	},
