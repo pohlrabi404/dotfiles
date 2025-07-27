88205455a4c2a2d466dfff36061db7b99cd2dee3
Pohlrabi <pohlrabi404@gmail.com>
2025-07-28 07:20:17 +0900

    m tabstop, format

--- home/.config/nvim/lua/settings/general.lua
+++ home/.config/nvim/lua/settings/general.lua
@@ -6,14 +6,19 @@ opt.number = true
 opt.fillchars = { eob = " " }
 
 opt.expandtab = false
-opt.tabstop = 8
+opt.tabstop = 4
 opt.shiftwidth = 4
 opt.softtabstop = 0
 
 opt.termguicolors = true
 
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.scrolloff = 10
+
 opt.guicursor = {
-	"n-v-c-sm:block", -- Normal, visual, command, showmatch: block cursor
+	"n-v-c-sm:block",                    -- Normal, visual, command, showmatch: block cursor
 	"i-ci-ve:block-blinkon500-blinkoff500", -- Insert modes: blinking block
-	"r-cr-o:hor20", -- Replace and operator-pending: horizontal bar
+	"r-cr-o:hor20",                      -- Replace and operator-pending: horizontal bar
 }
