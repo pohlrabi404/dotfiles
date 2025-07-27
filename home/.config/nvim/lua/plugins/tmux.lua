a82d3ae89e82f6eae0e4ead9cbc8ddf5efc2ef1a
Pohlrabi <pohlrabi404@gmail.com>
2025-07-28 07:13:38 +0900

    a tmux (hell)

---
+++ home/.config/nvim/lua/plugins/tmux.lua
@@ -0,0 +1,17 @@
+return {
+	"aserowy/tmux.nvim",
+	lazy = false,
+	opts = {
+		navigation = {
+			enable_default_keybindings = false,
+		},
+		resize = {
+			enable_default_keybindings = false,
+			resize_step_x = 5,
+			resize_step_y = 5,
+		},
+		swap = {
+			enable_default_keybindings = false,
+		},
+	},
+}
