38308cc242bfc0dcd6f99e58a5f977b0cc6a6968
Pohlrabi <pohlrabi404@gmail.com>
2025-07-28 07:15:12 +0900

    m reformat

--- home/.config/nvim/lua/plugins/formatter.lua
+++ home/.config/nvim/lua/plugins/formatter.lua
@@ -1,15 +1,17 @@
 return {
-    {
-	"stevearc/conform.nvim",
-	ft = { "lua" },
-	opts = {
-	    formatters_by_ft = {
-		lua = { "stylua" },
-	    },
-	    format_on_save = {
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	    }
+	{
+		"stevearc/conform.nvim",
+		ft = { "lua", "java", "arduino" },
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				java = { lsp_format = "prefer" },
+				arduino = {},
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "prefer",
+			},
+		},
 	},
-    },
 }
