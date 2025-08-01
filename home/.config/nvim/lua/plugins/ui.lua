cf005c23b9d1f58597f5d34a1a98771f794da975
Pohlrabi <pohlrabi404@gmail.com>
2025-08-02 08:24:19 +0900

    a ui, indent

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -22,4 +22,29 @@ return {
 		opts = { -- set to setup table
 		},
 	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "VeryLazy",
+		config = function()
+			vim.api.nvim_set_hl(0, "IndentBlanklineInactive", { fg = "#444444" })
+			vim.api.nvim_set_hl(0, "IndentBlanklineActive", { fg = "#888888" })
+
+			require("ibl").setup({
+				indent = {
+					char = "▏", -- NOTE: this is very left-aligned on meslo nerd font. different fonts may mess this up
+					highlight = {
+						"IndentBlanklineInactive",
+					},
+				},
+				scope = {
+					enabled = true,
+					show_start = false,
+					show_end = false,
+					highlight = {
+						"IndentBlanklineActive",
+					},
+				},
+			})
+		end,
+	},
 }
